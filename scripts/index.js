c6dade3fc5609080e7f8554d20ca26da7dc11d91
Vladimir Kumalagov <vkumalagov@icloud.com>
2024-02-13 17:28:18 +0400

    init: reproduce bug

---
+++ scripts/index.js
@@ -0,0 +1,22 @@
+const { compile, createFileManager } = require("@noir-lang/noir_wasm");
+const { resolve } = require("path");
+
+async function getCircuit(basePath) {
+    const fm = createFileManager(basePath);
+    const compiled = await compile(fm, basePath);
+    if (!("program" in compiled)) {
+        throw new Error("Compilation failed");
+    }
+    return compiled.program;
+}
+
+
+async function main() {
+    const basePath = resolve("circuits-noir");
+    await getCircuit(basePath);
+}
+
+main().catch((e) => {
+    console.error(e);
+    process.exit(1);
+})
